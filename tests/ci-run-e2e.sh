e13c2b0de91d4a5aa14e04ae2939ade7f8646546
Renaud Gaubert <rgaubert@nvidia.com>
2020-01-14 15:55:39 -0800

    Fix incorrect loop strategy in tests/ci-run-e2e.sh
    The current strategy was incorrectly waiting for the dcgm pod instead of
    waiting for the gpu validation pod then the dcgm pod.
    
    Signed-off-by: Renaud Gaubert <rgaubert@nvidia.com>

--- tests/ci-run-e2e.sh
+++ tests/ci-run-e2e.sh
@@ -41,17 +41,16 @@ kubectl apply -f gpu-pod.yaml
 
 rc=1
 while :; do
-	echo "Get all pods"
 	pods="$(kubectl get --all-namespaces pods -o json | jq '.items[] | {name: .metadata.name, ns: .metadata.namespace}' | jq -s -c .)"
-
-	echo "Checking GPU pod status"
 	status=$(kubectl get pods gpu-operator-test -o json | jq -r .status.phase)
 	if [ "${status}" = "Succeeded" ]; then
 		echo "GPU pod terminated successfully";
 		rc=0
 		break;
 	fi
-	echo "GPU pod status: ${status}";
+
+	# Echo useful information on stdout
+	kubectl get pods --all-namespaces
 
 	for pod in $(echo "$pods" | jq -r .[].name); do
 		ns=$(echo "$pods" | jq -r ".[] | select(.name == \"$pod\") | .ns")
@@ -65,21 +64,29 @@ while :; do
 	echo "------------------------------------------------" >> "${LOG_DIR}/cluster.logs"
 	kubectl get --all-namespaces pods >> "${LOG_DIR}/cluster.logs"
 
-	while :; do
-		echo "Checking gpu metrics"
-		dcgm_pod_status=$(kubectl get pods -lapp=nvidia-dcgm-exporter -n gpu-operator-resources -ojsonpath='{range .items[*]}{.status.phase}{"\n"}{end}')
-		if [ "${dcgm_pod_status}" = "Running" ]; then
-			dcgm_pod_ip=$(kubectl get pods -n gpu-operator-resources -o wide -l app=nvidia-dcgm-exporter | tail -n 1 | awk '{print $6}')
-			sleep 5
-			curl -s $dcgm_pod_ip:9400/gpu/metrics | grep "dcgm_gpu_temp"
-			break;
-		fi
-		echo "Sleeping 5 seconds"
-		sleep 5
-	done
-
 	echo "Sleeping 5 seconds"
 	sleep 5;
+done
+
+while :; do
+	echo "Checking dcgm pod status"
+	kubectl get pods -lapp=nvidia-dcgm-exporter -n gpu-operator-resources
+
+	dcgm_pod_status=$(kubectl get pods -lapp=nvidia-dcgm-exporter -n gpu-operator-resources -ojsonpath='{range .items[*]}{.status.phase}{"\n"}{end}')
+	if [ "${dcgm_pod_status}" = "Running" ]; then
+		# Sleep to give the gpu-exporter enough time to output it's metrics
+		# TODO need to add a readiness probe
+		sleep 5
+
+		dcgm_pod_ip=$(kubectl get pods -n gpu-operator-resources -o wide -l app=nvidia-dcgm-exporter | tail -n 1 | awk '{print $6}')
+		curl -s "$dcgm_pod_ip:9400/gpu/metrics" | grep "dcgm_gpu_temp"
+		rc=0;
+
+		break;
+	fi
+
+	echo "Sleeping 5 seconds"
+	sleep 5
 done
 
 exit $rc
