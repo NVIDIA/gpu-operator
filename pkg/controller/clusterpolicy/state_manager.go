a9e671a0d941dd91b6676eee07c93a0358dc332d
Kevin Pouget <kpouget@redhat.com>
2020-11-13 13:21:38 +0100

    state_manager: avoid accumulating states
    The method `ClusterPolicyController.init()` is called every time
    `ReconcileClusterPolicy.Reconcile` is called, many times.
    
    So `ClusterPolicyController.init()` should not call `addState` each
    time, otherwise `n.controls` and `n.resources` accumulate states.
    
    There is no visible bug caused by this issue, but the GPU-operator
    over-consumes memory, CPU, logs and APIServer resources because of it.
    
    Signed-off-by: Kevin Pouget <kpouget@redhat.com>

--- pkg/controller/clusterpolicy/state_manager.go
+++ pkg/controller/clusterpolicy/state_manager.go
@@ -153,21 +153,23 @@ func (n *ClusterPolicyController) init(r *ReconcileClusterPolicy, i *gpuv1.Clust
 	n.rec = r
 	n.idx = 0
 
-	promv1.AddToScheme(r.scheme)
-	secv1.AddToScheme(r.scheme)
+	if len(n.controls) == 0 {
+		promv1.AddToScheme(r.scheme)
+		secv1.AddToScheme(r.scheme)
+
+		addState(n, "/opt/gpu-operator/gpu-feature-discovery")
+		addState(n, "/opt/gpu-operator/state-driver")
+		addState(n, "/opt/gpu-operator/state-container-toolkit")
+		addState(n, "/opt/gpu-operator/state-device-plugin")
+		addState(n, "/opt/gpu-operator/state-device-plugin-validation")
+		addState(n, "/opt/gpu-operator/state-monitoring")
+	}
 
 	// fetch all nodes and label gpu nodes
 	err = n.labelGPUNodes()
 	if err != nil {
 		return err
 	}
-
-	addState(n, "/opt/gpu-operator/gpu-feature-discovery")
-	addState(n, "/opt/gpu-operator/state-driver")
-	addState(n, "/opt/gpu-operator/state-container-toolkit")
-	addState(n, "/opt/gpu-operator/state-device-plugin")
-	addState(n, "/opt/gpu-operator/state-device-plugin-validation")
-	addState(n, "/opt/gpu-operator/state-monitoring")
 
 	return nil
 }
