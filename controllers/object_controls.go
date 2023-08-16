86a65933510595959cd16ff7b16bf056e8120322
Tariq Ibrahim <tibrahim@nvidia.com>
2023-08-15 18:18:13 -0700

    do not set the RHEL_VERSION envar in gpu-operator
    remove all references to RHEL_VERSION

--- controllers/object_controls.go
+++ controllers/object_controls.go
@@ -2813,7 +2813,9 @@ func createEmptyDirVolume(volumeName string) corev1.Volume {
 func transformDriverContainer(obj *appsv1.DaemonSet, config *gpuv1.ClusterPolicySpec, n ClusterPolicyController) error {
 	driverIndex := 0
 	driverCtrFound := false
-	for i, container := range obj.Spec.Template.Spec.Containers {
+
+	podSpec := &obj.Spec.Template.Spec
+	for i, container := range podSpec.Containers {
 		// check if this is the main nvidia-driver container
 		if container.Name == "nvidia-driver-ctr" {
 			driverIndex = i
@@ -2826,70 +2828,72 @@ func transformDriverContainer(obj *appsv1.DaemonSet, config *gpuv1.ClusterPolicy
 		return fmt.Errorf("driver container (nvidia-driver-ctr) is missing from the driver daemonset manifest")
 	}
 
+	driverContainer := &podSpec.Containers[driverIndex]
+
 	image, err := resolveDriverTag(n, &config.Driver)
 	if err != nil {
 		return err
 	}
 	if image != "" {
-		obj.Spec.Template.Spec.Containers[driverIndex].Image = image
+		driverContainer.Image = image
 	}
 
 	// update image pull policy
-	obj.Spec.Template.Spec.Containers[driverIndex].ImagePullPolicy = gpuv1.ImagePullPolicy(config.Driver.ImagePullPolicy)
+	driverContainer.ImagePullPolicy = gpuv1.ImagePullPolicy(config.Driver.ImagePullPolicy)
 
 	// set image pull secrets
 	if len(config.Driver.ImagePullSecrets) > 0 {
 		for _, secret := range config.Driver.ImagePullSecrets {
-			obj.Spec.Template.Spec.ImagePullSecrets = append(obj.Spec.Template.Spec.ImagePullSecrets, v1.LocalObjectReference{Name: secret})
+			podSpec.ImagePullSecrets = append(podSpec.ImagePullSecrets, v1.LocalObjectReference{Name: secret})
 		}
 	}
 	// set resource limits
 	if config.Driver.Resources != nil {
-		obj.Spec.Template.Spec.Containers[driverIndex].Resources.Requests = config.Driver.Resources.Requests
-		obj.Spec.Template.Spec.Containers[driverIndex].Resources.Limits = config.Driver.Resources.Limits
+		driverContainer.Resources.Requests = config.Driver.Resources.Requests
+		driverContainer.Resources.Limits = config.Driver.Resources.Limits
 	}
 	// set arguments if specified for driver container
 	if len(config.Driver.Args) > 0 {
-		obj.Spec.Template.Spec.Containers[driverIndex].Args = config.Driver.Args
+		driverContainer.Args = config.Driver.Args
 	}
 	// set/append environment variables for exporter container
 	if len(config.Driver.Env) > 0 {
 		for _, env := range config.Driver.Env {
-			setContainerEnv(&(obj.Spec.Template.Spec.Containers[driverIndex]), env.Name, env.Value)
+			setContainerEnv(driverContainer, env.Name, env.Value)
 		}
 	}
 	// set container probe timeouts
 	if config.Driver.StartupProbe != nil {
-		setContainerProbe(&(obj.Spec.Template.Spec.Containers[driverIndex]), config.Driver.StartupProbe, Startup)
+		setContainerProbe(driverContainer, config.Driver.StartupProbe, Startup)
 	}
 	if config.Driver.LivenessProbe != nil {
-		setContainerProbe(&(obj.Spec.Template.Spec.Containers[driverIndex]), config.Driver.LivenessProbe, Liveness)
+		setContainerProbe(driverContainer, config.Driver.LivenessProbe, Liveness)
 	}
 	if config.Driver.ReadinessProbe != nil {
-		setContainerProbe(&(obj.Spec.Template.Spec.Containers[driverIndex]), config.Driver.ReadinessProbe, Readiness)
+		setContainerProbe(driverContainer, config.Driver.ReadinessProbe, Readiness)
 	}
 
 	if config.Driver.GPUDirectRDMA != nil && config.Driver.GPUDirectRDMA.IsEnabled() {
 		// set env indicating nvidia-peermem is enabled to compile module with required ib_* interfaces
-		setContainerEnv(&(obj.Spec.Template.Spec.Containers[driverIndex]), GPUDirectRDMAEnabledEnvName, "true")
+		setContainerEnv(driverContainer, GPUDirectRDMAEnabledEnvName, "true")
 		// check if MOFED drives are directly installed on host and update source path accordingly
 		// to build nvidia-peermem module
 		if config.Driver.GPUDirectRDMA.UseHostMOFED != nil && *config.Driver.GPUDirectRDMA.UseHostMOFED {
 			// mount /usr/src/ofa_kernel path directly from host to build using MOFED drivers installed on host
-			for index, volume := range obj.Spec.Template.Spec.Volumes {
+			for index, volume := range podSpec.Volumes {
 				if volume.Name == "mlnx-ofed-usr-src" {
-					obj.Spec.Template.Spec.Volumes[index].HostPath.Path = "/usr/src"
+					podSpec.Volumes[index].HostPath.Path = "/usr/src"
 				}
 			}
 			// set env indicating host-mofed is enabled
-			setContainerEnv(&(obj.Spec.Template.Spec.Containers[driverIndex]), UseHostMOFEDEnvName, "true")
+			setContainerEnv(driverContainer, UseHostMOFEDEnvName, "true")
 		}
 	}
 
 	// set any licensing configuration required
 	if config.Driver.LicensingConfig != nil && config.Driver.LicensingConfig.ConfigMapName != "" {
 		licensingConfigVolMount := corev1.VolumeMount{Name: "licensing-config", ReadOnly: true, MountPath: VGPULicensingConfigMountPath, SubPath: VGPULicensingFileName}
-		obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts = append(obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts, licensingConfigVolMount)
+		driverContainer.VolumeMounts = append(driverContainer.VolumeMounts, licensingConfigVolMount)
 
 		// gridd.conf always mounted
 		licenseItemsToInclude := []corev1.KeyToPath{
@@ -2905,7 +2909,7 @@ func transformDriverContainer(obj *appsv1.DaemonSet, config *gpuv1.ClusterPolicy
 				Path: NLSClientTokenFileName,
 			})
 			nlsTokenVolMount := corev1.VolumeMount{Name: "licensing-config", ReadOnly: true, MountPath: NLSClientTokenMountPath, SubPath: NLSClientTokenFileName}
-			obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts = append(obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts, nlsTokenVolMount)
+			driverContainer.VolumeMounts = append(driverContainer.VolumeMounts, nlsTokenVolMount)
 		}
 
 		licensingConfigVolumeSource := corev1.VolumeSource{
@@ -2917,13 +2921,13 @@ func transformDriverContainer(obj *appsv1.DaemonSet, config *gpuv1.ClusterPolicy
 			},
 		}
 		licensingConfigVol := corev1.Volume{Name: "licensing-config", VolumeSource: licensingConfigVolumeSource}
-		obj.Spec.Template.Spec.Volumes = append(obj.Spec.Template.Spec.Volumes, licensingConfigVol)
+		podSpec.Volumes = append(podSpec.Volumes, licensingConfigVol)
 	}
 
 	// set virtual topology daemon configuration if specified for vGPU driver
 	if config.Driver.VirtualTopology != nil && config.Driver.VirtualTopology.Config != "" {
 		topologyConfigVolMount := corev1.VolumeMount{Name: "topology-config", ReadOnly: true, MountPath: VGPUTopologyConfigMountPath, SubPath: VGPUTopologyConfigFileName}
-		obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts = append(obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts, topologyConfigVolMount)
+		driverContainer.VolumeMounts = append(driverContainer.VolumeMounts, topologyConfigVolMount)
 
 		topologyConfigVolumeSource := corev1.VolumeSource{
 			ConfigMap: &corev1.ConfigMapVolumeSource{
@@ -2939,7 +2943,7 @@ func transformDriverContainer(obj *appsv1.DaemonSet, config *gpuv1.ClusterPolicy
 			},
 		}
 		topologyConfigVol := corev1.Volume{Name: "topology-config", VolumeSource: topologyConfigVolumeSource}
-		obj.Spec.Template.Spec.Volumes = append(obj.Spec.Template.Spec.Volumes, topologyConfigVol)
+		podSpec.Volumes = append(podSpec.Volumes, topologyConfigVol)
 	}
 
 	// mount any custom kernel module configuration parameters at /drivers
@@ -2949,8 +2953,8 @@ func transformDriverContainer(obj *appsv1.DaemonSet, config *gpuv1.ClusterPolicy
 		if err != nil {
 			return fmt.Errorf("ERROR: failed to create ConfigMap VolumeMounts for kernel module configuration: %v", err)
 		}
-		obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts = append(obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts, volumeMounts...)
-		obj.Spec.Template.Spec.Volumes = append(obj.Spec.Template.Spec.Volumes, createConfigMapVolume(config.Driver.KernelModuleConfig.Name, itemsToInclude))
+		driverContainer.VolumeMounts = append(driverContainer.VolumeMounts, volumeMounts...)
+		podSpec.Volumes = append(podSpec.Volumes, createConfigMapVolume(config.Driver.KernelModuleConfig.Name, itemsToInclude))
 	}
 
 	// no further repo configuration required when using pre-compiled drivers, return here.
@@ -2968,8 +2972,8 @@ func transformDriverContainer(obj *appsv1.DaemonSet, config *gpuv1.ClusterPolicy
 		if err != nil {
 			return fmt.Errorf("ERROR: failed to create ConfigMap VolumeMounts for custom repo config: %v", err)
 		}
-		obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts = append(obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts, volumeMounts...)
-		obj.Spec.Template.Spec.Volumes = append(obj.Spec.Template.Spec.Volumes, createConfigMapVolume(config.Driver.RepoConfig.ConfigMapName, itemsToInclude))
+		driverContainer.VolumeMounts = append(driverContainer.VolumeMounts, volumeMounts...)
+		podSpec.Volumes = append(podSpec.Volumes, createConfigMapVolume(config.Driver.RepoConfig.ConfigMapName, itemsToInclude))
 	}
 
 	// set any custom ssl key/certificate configuration provided
@@ -2982,8 +2986,8 @@ func transformDriverContainer(obj *appsv1.DaemonSet, config *gpuv1.ClusterPolicy
 		if err != nil {
 			return fmt.Errorf("ERROR: failed to create ConfigMap VolumeMounts for custom certs: %v", err)
 		}
-		obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts = append(obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts, volumeMounts...)
-		obj.Spec.Template.Spec.Volumes = append(obj.Spec.Template.Spec.Volumes, createConfigMapVolume(config.Driver.CertConfig.Name, itemsToInclude))
+		driverContainer.VolumeMounts = append(driverContainer.VolumeMounts, volumeMounts...)
+		podSpec.Volumes = append(podSpec.Volumes, createConfigMapVolume(config.Driver.CertConfig.Name, itemsToInclude))
 	}
 
 	release, err := parseOSRelease()
@@ -3014,10 +3018,10 @@ func transformDriverContainer(obj *appsv1.DaemonSet, config *gpuv1.ClusterPolicy
 				MountPath: mountPath,
 				ReadOnly:  true,
 			}
-			obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts = append(obj.Spec.Template.Spec.Containers[driverIndex].VolumeMounts, volMountSubscription)
+			driverContainer.VolumeMounts = append(driverContainer.VolumeMounts, volMountSubscription)
 
 			subscriptionVol := corev1.Volume{Name: volMountSubscriptionName, VolumeSource: pathToVolumeSource[mountPath]}
-			obj.Spec.Template.Spec.Volumes = append(obj.Spec.Template.Spec.Volumes, subscriptionVol)
+			podSpec.Volumes = append(podSpec.Volumes, subscriptionVol)
 		}
 	}
 
@@ -3026,16 +3030,12 @@ func transformDriverContainer(obj *appsv1.DaemonSet, config *gpuv1.ClusterPolicy
 		return nil
 	}
 
-	// Add env vars needed by nvidia-driver to enable the right releasever and EUS rpm repos
-	rhelVersion := corev1.EnvVar{Name: "RHEL_VERSION", Value: release["RHEL_VERSION"]}
 	ocpVersion := corev1.EnvVar{Name: "OPENSHIFT_VERSION", Value: release["OPENSHIFT_VERSION"]}
-
-	obj.Spec.Template.Spec.Containers[driverIndex].Env = append(obj.Spec.Template.Spec.Containers[driverIndex].Env, rhelVersion)
-	obj.Spec.Template.Spec.Containers[driverIndex].Env = append(obj.Spec.Template.Spec.Containers[driverIndex].Env, ocpVersion)
+	driverContainer.Env = append(driverContainer.Env, ocpVersion)
 
 	// Automatically apply proxy settings for OCP and inject custom CA if configured by user
 	// https://docs.openshift.com/container-platform/4.6/networking/configuring-a-custom-pki.html
-	err = applyOCPProxySpec(n, &obj.Spec.Template.Spec)
+	err = applyOCPProxySpec(n, podSpec)
 	if err != nil {
 		return err
 	}
@@ -3096,7 +3096,6 @@ func transformVGPUManagerContainer(obj *appsv1.DaemonSet, config *gpuv1.ClusterP
 	// add env for OCP
 	if _, ok := release["OPENSHIFT_VERSION"]; ok {
 		setContainerEnv(container, "OPENSHIFT_VERSION", release["OPENSHIFT_VERSION"])
-		setContainerEnv(container, "RHEL_VERSION", release["RHEL_VERSION"])
 	}
 
 	return nil
