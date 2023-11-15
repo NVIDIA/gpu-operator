9e09bf8f6e7a7afdd32023c15377f2484ada6ddd
Tariq Ibrahim <tibrahim@nvidia.com>
2023-11-15 08:34:24 -0800

    fix minor shellcheck issues and typos in e2e tests

--- tests/scripts/end-to-end-nvidia-driver.sh
+++ tests/scripts/end-to-end-nvidia-driver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source ${SCRIPT_DIR}/.definitions.sh
+source "${SCRIPT_DIR}"/.definitions.sh
 
 echo ""
 echo ""
@@ -9,24 +9,24 @@ echo "--------------Installing the GPU Operator with NvidiaDriverCR Enabled-----
 echo "-----------------------------------------------------------------------------------"
 # Install the operator with nvidiaDriver mode set to true
 OPERATOR_OPTIONS="--set driver.nvidiaDriverCRD.enabled=true" ${SCRIPT_DIR}/install-operator.sh
-USE_NVIDIA_DRIVER_CR="true" ${SCRIPT_DIR}/verify-operator.sh
-USE_NVIDIA_DRIVER_CR="true" ${SCRIPT_DIR}/verify-operand-restarts.sh
+USE_NVIDIA_DRIVER_CR="true" "${SCRIPT_DIR}"/verify-operator.sh
+USE_NVIDIA_DRIVER_CR="true" "${SCRIPT_DIR}"/verify-operand-restarts.sh
 
 # Install a workload and verify that this works as expected
-${SCRIPT_DIR}/install-workload.sh
-${SCRIPT_DIR}/verify-workload.sh
+"${SCRIPT_DIR}"/install-workload.sh
+"${SCRIPT_DIR}"/verify-workload.sh
 
 echo ""
 echo ""
 echo "----------------------------Updating the NvidiaDriverCR----------------------------"
 echo "-----------------------------------------------------------------------------------"
 # Test updates of the NvidiaDriver custom resource
-${SCRIPT_DIR}/update-nvidiadriver.sh
+"${SCRIPT_DIR}"/update-nvidiadriver.sh
 
 echo ""
 echo ""
 echo "--------------------------------------Teardown--------------------------------------"
 echo "------------------------------------------------------------------------------------"
 # Uninstall the workload and operator
-${SCRIPT_DIR}/uninstall-workload.sh
-${SCRIPT_DIR}/uninstall-operator.sh
+"${SCRIPT_DIR}"/uninstall-workload.sh
+"${SCRIPT_DIR}"/uninstall-operator.sh
--- tests/scripts/prerequisites.sh
+++ tests/scripts/prerequisites.sh
@@ -6,7 +6,7 @@ if [[ "${SKIP_PREREQUISITES}" == "true" ]]; then
 fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-source ${SCRIPT_DIR}/.definitions.sh
+source "${SCRIPT_DIR}"/.definitions.sh
 
 echo "Create log dir ${LOG_DIR}"
 mkdir -p "${LOG_DIR}"
