22f41d137f82b636cdd16ee1549e450c19348287
Christopher Desiniotis <cdesiniotis@nvidia.com>
2024-04-29 11:02:45 -0700

    Bump project version to 24.3.0
    Signed-off-by: Christopher Desiniotis <cdesiniotis@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -17,7 +17,7 @@
 # To re-generate a bundle for another specific version without changing the standard setup, you can:
 # - use the VERSION as arg of the bundle target (e.g make bundle VERSION=0.0.2)
 # - use environment variables to overwrite this value (e.g export VERSION=0.0.2)
-VERSION ?= v23.9.2
+VERSION ?= v24.3.0
 
 CUDA_VERSION ?= 12.4.1
 GOLANG_VERSION ?= 1.22.2
