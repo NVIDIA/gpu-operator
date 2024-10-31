b2b990b33251ef56dfe079dfb932325ba88e4ac7
Tariq Ibrahim <tibrahim@nvidia.com>
2024-10-30 20:00:38 -0700

    add support for driver 565.57.01
    Signed-off-by: Tariq Ibrahim <tibrahim@nvidia.com>

--- tests/scripts/.definitions.sh
+++ tests/scripts/.definitions.sh
@@ -17,7 +17,7 @@ TERRAFORM="terraform -chdir=${TERRAFORM_DIR}"
 : ${LOG_DIR:="/tmp/logs"}
 : ${PROJECT:="$(basename "${PROJECT_DIR}")"}
 : ${TEST_NAMESPACE:="test-operator"}
-: ${TARGET_DRIVER_VERSION:="560.35.03"}
+: ${TARGET_DRIVER_VERSION:="565.57.01"}
 
 : ${OPERATOR_IMAGE:="nvcr.io/nvidia/gpu-operator"}
 
