a17104afb876c9fbd8bb5ab247e3a1d37e95d4d7
Tariq Ibrahim <tibrahim@nvidia.com>
2024-12-10 12:11:49 -0600

    bump go to 1.23.4 and golangci-lint to 1.62.2
    Signed-off-by: Tariq Ibrahim <tibrahim@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -19,8 +19,8 @@
 # - use environment variables to overwrite this value (e.g export VERSION=0.0.2)
 VERSION ?= v24.9.1
 
-GOLANG_VERSION ?= 1.23.3
+GOLANG_VERSION ?= 1.23.4
 
-GOLANGCI_LINT_VERSION ?= v1.62.0
+GOLANGCI_LINT_VERSION ?= v1.62.2
 
 GIT_COMMIT ?= $(shell git describe --match="" --dirty --long --always 2> /dev/null || echo "")
