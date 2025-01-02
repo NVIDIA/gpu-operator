60ad4f0623461c093855529eda22b139cf981caf
Tariq Ibrahim <tibrahim@nvidia.com>
2025-01-02 09:47:28 -0800

    bump golangci-lint to v1.63.2
    Signed-off-by: Tariq Ibrahim <tibrahim@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -21,6 +21,6 @@ VERSION ?= v24.9.1
 
 GOLANG_VERSION ?= 1.23.4
 
-GOLANGCI_LINT_VERSION ?= v1.62.2
+GOLANGCI_LINT_VERSION ?= v1.63.2
 
 GIT_COMMIT ?= $(shell git describe --match="" --dirty --long --always 2> /dev/null || echo "")
