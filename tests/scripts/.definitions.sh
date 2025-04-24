e3767744ebd7ad75d5b3781c9a2c4675987cebb5
Tariq Ibrahim <tibrahim@nvidia.com>
2025-04-23 23:08:56 -0700

    update driver versions to 570.133.20 550.163.01 & 535.247.01
    Signed-off-by: Tariq Ibrahim <tibrahim@nvidia.com>
    (cherry picked from commit 7f972a3047cfcba2f91c413c40a403bd19dbfdbe)

--- tests/scripts/.definitions.sh
+++ tests/scripts/.definitions.sh
@@ -17,7 +17,7 @@ TERRAFORM="terraform -chdir=${TERRAFORM_DIR}"
 : ${LOG_DIR:="/tmp/logs"}
 : ${PROJECT:="$(basename "${PROJECT_DIR}")"}
 : ${TEST_NAMESPACE:="test-operator"}
-: ${TARGET_DRIVER_VERSION:="550.144.03"}
+: ${TARGET_DRIVER_VERSION:="550.163.01"}
 
 : ${OPERATOR_IMAGE:="nvcr.io/nvidia/gpu-operator"}
 
