fc1f62e064c3f20099ac804b6e4197eb0dc31047
Christopher Desiniotis <cdesiniotis@nvidia.com>
2025-07-23 16:14:20 -0700

    Bump golang version to v1.24.5
    Signed-off-by: Christopher Desiniotis <cdesiniotis@nvidia.com>

--- versions.mk
+++ versions.mk
@@ -19,7 +19,7 @@
 # - use environment variables to overwrite this value (e.g export VERSION=0.0.2)
 VERSION ?= v25.3.2
 
-GOLANG_VERSION ?= 1.24.3
+GOLANG_VERSION ?= 1.24.5
 
 GOLANGCI_LINT_VERSION ?= v1.64.7
 
