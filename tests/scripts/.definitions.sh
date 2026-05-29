bc8d643489c653a9b2e58ed66c1cfbfef859f2fa
Tariq Ibrahim <tibrahim@nvidia.com>
2026-05-29 11:06:38 -0700

    [ci-e2e] update TARGET_DRIVER_VERSION
    The R535 driver branch transitions to EOL as of June 2026. Given this,
    we bump the TARGET_DRIVER_VERSION to use a driver version from the oldest
    driver branch that is still actively supported.
    
    Signed-off-by: Tariq Ibrahim <tibrahim@nvidia.com>

--- tests/scripts/.definitions.sh
+++ tests/scripts/.definitions.sh
@@ -17,7 +17,7 @@ TERRAFORM="terraform -chdir=${TERRAFORM_DIR}"
 : ${LOG_DIR:="/tmp/logs"}
 : ${PROJECT:="$(basename "${PROJECT_DIR}")"}
 : ${TEST_NAMESPACE:="test-operator"}
-: ${TARGET_DRIVER_VERSION:="535.288.01"}
+: ${TARGET_DRIVER_VERSION:="580.159.04"}
 
 : ${OPERATOR_IMAGE:="nvcr.io/nvidia/gpu-operator"}
 
